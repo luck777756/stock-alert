59c166a716bba661feb57c0b6afa9a3b213bab50
luck777756 <thswldnd0900@gmail.com>
2025-04-20 23:44:17 +0900

    utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
-
-import pandas as pd
 import numpy as np
+import pandas as pd
 from ta.volatility import BollingerBands
 from ta.trend import ADXIndicator
 
@@ -17,7 +16,7 @@ def calculate_score(df):
     bb = BollingerBands(close=df["Close"], window=20, window_dev=2)
      pb = bb.bollinger_pband()
     if isinstance(pb, pd.DataFrame) or (hasattr(pb, 'values') and isinstance(pb.values, np.ndarray) and pb.values.ndim > 1):
-        pb = pd.Series(pb.values.flatten())
+    pb = pd.Series(pb.values.flatten())
     pb = pb.fillna(0)
     bb_score = pb.iloc[-1] if not pb.empty else 0
     return round((price_now - ma20) / ma20 + volume_score + adx / 100 + bb_score, 4)
